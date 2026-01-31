f75debbaa3e6c7ec80f9c6707df1b1f95323c03e
Roman Fedoriv <fedoriv.roman.v@gmail.com>
2026-01-31 16:54:48 +0200

    create axios instance

---
+++ src/lib/api/axios.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
